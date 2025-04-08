233d9692f89eee874718406c20d8cfa6e99d3b5b
xergon <christoffer.isenberg@gmail.com>
2025-04-08 16:11:51 +0200

    Added DbContext, Generic repo, repo example

---
+++ FribergHome-API/Data/Repositories/PropertyRepository.cs
@@ -0,0 +1,19 @@
+﻿using FribergHome_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FribergHome_API.Data.Repositories
+{
+    // Author: Christoffer
+    public class PropertyRepository : GenericRepository<Property, ApplicationDbContext>, IPropertyRepository
+    {
+        public PropertyRepository(ApplicationDbContext dbContext) : base(dbContext) {}
+
+        public async Task<ICollection<Property>?> FindPropertyInMuncipality(Muncipality muncipality)
+        {
+            return await DbContext
+                .Properties
+                .Where(e => e.Muncipality == muncipality)
+                .ToListAsync();
+        }
+    }
+}
